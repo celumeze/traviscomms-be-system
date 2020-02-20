1a5fff65d521065611803e6d32c43a81e77212f1
charleselumeze <charleselumeze@gmail.com>
2020-02-20 23:36:31 +0000

    subscription type configuration created

---
+++ FlexiComms.Data.Repository/Configuration/SubscriptionTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FlexiComms.Data.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexiComms.Data.Repository.Configuration
+{
+    public class SubscriptionTypeConfiguration : IEntityTypeConfiguration<SubscriptionType>
+    {
+        public void Configure(EntityTypeBuilder<SubscriptionType> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(20).IsRequired().IsUnicode(false);
+        }
+    }
+}
