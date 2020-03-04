2812d77df0c3fe8a6dbe4b2d918a40e1365dd278
charleselumezev1 <charles.elumeze@version1.com>
2020-03-04 16:13:43 +0000

    added registration dto in API project

---
+++ FlexiComms.Api/Dto/ClientDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexiComms.Api.Dto
+{
+    public class ClientDto
+    {
+        public Client()
+        {
+            Companies = new List<Company>();
+        }
+        public Guid ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Active { get; set; }
+        public Guid SubscriptionTypeId { get; set; }
+        public Guid ClientRoleId { get; set; }
+
+        public List<CompanyDto> Companies { get; set; }
+        public ClientServiceProviderDto ClientServiceProvider { get; set; }
+        public SubscriptionTypeDto SubscriptionType { get; set; }
+        public ClientRoleDto ClientRole { get; set; }
+    }
+}
